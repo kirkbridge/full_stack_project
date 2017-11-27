c0c175f7fa70783ecb12f588f2821916cfaf46dd
kirkbridge <bkirkbridetaylor@gmail.com>
2017-11-27 09:00:13 -0600

    Created session controller and added login page, also added link to login page to nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
+
   resources :users, except: %i[index, destroy]
 
   get '/signup', to: 'users#new'
